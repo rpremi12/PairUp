c783f1e3bec8df0efb1231e8429bcc14b3a5dabe
Ryan <rpremi@hotmail.com>
2019-07-07 18:13:12 -0700

    Location Complete

---
+++ extra_jsons.js
@@ -0,0 +1,88 @@
+//SPARE JSONS
+//GetJson 
+function g_json(){
+	return {
+	   "html_attributions" : [],
+	   "results" : [
+	      {
+	         "formatted_address" : "Portland, OR, USA",
+	         "geometry" : {
+	            "location" : {
+	               "lat" : 45.5154586,
+	               "lng" : -122.6793461
+	            },
+	            "viewport" : {
+	               "northeast" : {
+	                  "lat" : 45.65288049999999,
+	                  "lng" : -122.472024
+	               },
+	               "southwest" : {
+	                  "lat" : 45.4325358,
+	                  "lng" : -122.8367493
+	               }
+	            }
+	         },
+	         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png",
+	         "id" : "a4bab2d2863eff36d4e6bd34db32ba7a3b000b22",
+	         "name" : "Portland",
+	         "photos" : [
+	            {
+	               "height" : 3205,
+	               "html_attributions" : [
+	                  "\u003ca href=\"https://maps.google.com/maps/contrib/116762854930395036579/photos\"\u003eAndrew Gregor\u003c/a\u003e"
+	               ],
+	               "photo_reference" : "CmRaAAAAUjGQS9_fKg44CAhmk8HDsBc9zSeN6Wf0zICepasrveLlDF_IaeoZVs35i7xDc0HdL-gCbGLWWQHwMyopC7vfveihUVh1tXG5W8IEt1O9qn7rdvYlEwQfc3H5bBJXaoRaEhCGklnQj9YsPxMLxRQ8JMtFGhRIdSv-AMdvH6sgczPexf_jrC_Fiw",
+	               "width" : 4807
+	            }
+	         ],
+	         "place_id" : "ChIJJ3SpfQsLlVQRkYXR9ua5Nhw",
+	         "reference" : "ChIJJ3SpfQsLlVQRkYXR9ua5Nhw",
+	         "types" : [ "locality", "political" ]
+	      }
+	   ],
+	   "status" : "OK"
+	};
+}
+function g_json2(){
+return {
+   "html_attributions" : [],
+   "results" : [
+      {
+         "formatted_address" : "San Jose, CA, USA",
+         "geometry" : {
+            "location" : {
+               "lat" : 37.3382082,
+               "lng" : -121.8863286
+            },
+            "viewport" : {
+               "northeast" : {
+                  "lat" : 37.4692304,
+                  "lng" : -121.589154
+               },
+               "southwest" : {
+                  "lat" : 37.124493,
+                  "lng" : -122.0456719
+               }
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png",
+         "id" : "c00d0e6ed23a5a6b418841457b33c47430a93a4c",
+         "name" : "San Jose",
+         "photos" : [
+            {
+               "height" : 2448,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/104500663303866676451/photos\"\u003eA Google User\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRaAAAAuJDoU2a9Bi_sa5C2YhaP-Bq-ZoNqadO8XrVdfkc2s_3Hq952dpetEchKRnLut6oDccyAy5haOqZcFXjGhhLhhq2K7nFTxqx5wAwyd4eZeypWjiDa008IUhHMux6rK5AHEhDbsAFtgsBhHepDTZayUgvwGhTcnWZz6UXSvXimT1wvUbYY7zJ96Q",
+               "width" : 3264
+            }
+         ],
+         "place_id" : "ChIJ9T_5iuTKj4ARe3GfygqMnbk",
+         "reference" : "ChIJ9T_5iuTKj4ARe3GfygqMnbk",
+         "types" : [ "locality", "political" ]
+      }
+   ],
+   "status" : "OK"
+}
+}
